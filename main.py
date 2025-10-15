9d0ac3d1fb5908b6522450e648f828ef7bf68eb6
Nathaniel Rogalskyj <nr282@cornell.edu>
2025-10-15 00:00:35 -0400

    Add fit_residential to main

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ How are we going to handle configuration.
 import pandas as pd
 from config import config
 from data import data_preparation
+from models.residential import fit_residential_model
 
 def read_configuration():
     """
@@ -31,11 +32,20 @@ def read_configuration():
 def main():
 
 
-    config = read_configuration()
-    datasets = data_preparation.gather_relevant_data(config)
+    # Run the fitting of the residential model for Virginia.
+    # TODO: Currently running with the global optimization.
 
+    start_training_time = "2022-01-01"
+    end_training_time = "2023-12-31"
+    start_test_time = "2022-01-01"
+    end_test_time = "2023-01-01"
+    state = "Virginia"
 
-
+    fit_residential_model(start_training_time,
+                          end_training_time,
+                          start_test_time,
+                          end_test_time,
+                          state)
 
 
 if __name__ == "__main__":
