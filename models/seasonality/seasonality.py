80f5fbe361de67e260289aa03463949e3a4fb5bf
Nathaniel Rogalskyj <nr282@cornell.edu>
2025-12-08 14:36:35 -0500

    Add seasonality

--- models/seasonality/seasonality.py
+++ models/seasonality/seasonality.py
@@ -7,13 +7,46 @@ Seasonality will be addressed via adding of a seasonal time series.
 
 import numpy as np
 import pandas as pd
+import datetime
+
+def get_day_of_year(date_str: str):
+
+    date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+    day_of_year = date_obj.timetuple().tm_yday
+    return day_of_year
+
+def get_cosine_1_year_period_value(day_of_year):
+
+    return np.cos(2 * np.pi * day_of_year / 365)
+
+def get_cosine_6_month_period_value(day_of_year):
+
+    return np.cos(2 * np.pi * day_of_year / 182)
 
 def get_time_series_1(dates):
-    pass
+
+    time_vals = list(map(lambda x: get_cosine_1_year_period_value(get_day_of_year(x)), dates))
+    time_val = np.array(time_vals)
+    return time_vals
 
 
 def get_time_series_2(dates):
-    pass
+
+    time_vals = list(map(lambda x: get_cosine_6_month_period_value(get_day_of_year(x)), dates))
+    time_val = np.array(time_vals)
+    return time_vals
+
+def example():
+    from datetime import datetime
+
+    # Example date: February 7, 2023
+    year, month, day = 2023, 2, 7
+    date_obj = datetime(year, month, day)
+    day_of_year = date_obj.timetuple().tm_yday
+
+    # Get the day of the year
+    print(f"The day of the year for {date_obj.strftime('%Y-%m-%d')} is: {day_of_year}")
 
 
-
+if __name__ == "__main__":
+    example()
--- utils.py
+++ utils.py
@@ -1,5 +1,18 @@
+import os
 
 
 def get_base_path():
 
-    return "C:\\Users\\nr282\\PycharmProjects\\PythonProject4"
+    potential_paths = ["C:\\Users\\nr282\\PycharmProjects\\PythonProject4",
+                       "/home/ec2-user/natural_gas_consumption"]
+
+    actual_path = None
+    for pot_path in potential_paths:
+        if os.path.exists(pot_path):
+            actual_path = pot_path
+            break
+
+    if actual_path is None:
+        raise ValueError("Could not find base path")
+
+    return actual_path
